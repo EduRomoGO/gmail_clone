f294aabf249bfafa281de8c9bc4cd3791a640439
EduRomoGO <eduromogo@outlook.com>
2014-11-30 21:57:09 +0100

    various changes
    Changes in html,.
    Changes in email.rb // now it retrieve the correct mails.
    Visual changes with css.

--- email.rb
+++ email.rb
@@ -8,10 +8,10 @@ set :bind, '0.0.0.0'
 
 
 @@emails = {
-        "1234" => { "from" => "Charlie Crews", "subject" => "I'm back", "preview"=>"After been 12 years in prison...", "starred"=>"true" },
-        "2345" => { "from" => "Jack Gallagher", "subject" => "The psiquiatric revolution", "preview"=>"I want to change the way this hospital...", "starred"=>"false" },
-        "3456" => { "from" => "Richard Castle", "subject" => "Niki Heat", "preview"=>"The detective Niki Heat....", "starred"=>"true" },
-        "4567" => { "from" => "Sheldon Cooper", "subject" => "I'm going to win the Noble price", "preview"=>"Dear poor minds, probably you...", "starred"=>"false" }
+        1234 => { "from" => "Charlie Crews", "subject" => "I'm back", "preview"=>"After been 12 years in prison...", "starred"=>"true" },
+        2345 => { "from" => "Jack Gallagher", "subject" => "The psiquiatric revolution", "preview"=>"I want to change the way this hospital...", "starred"=>"false" },
+        3456 => { "from" => "Richard Castle", "subject" => "Niki Heat", "preview"=>"The detective Niki Heat....", "starred"=>"true" },
+        4567 => { "from" => "Sheldon Cooper", "subject" => "I'm going to win the Noble price", "preview"=>"Dear poor minds, probably you...", "starred"=>"false" }
     }
 
 
@@ -39,7 +39,8 @@ get '/email/new' do
 end
 
 get '/email/:id' do
-	emailToSend = (@@emails[:id]) ? @@emails[:id] : createEmail
+    id = params[:id].to_i
+	emailToSend = (@@emails[id]) ? @@emails[id] : createEmail
     emailToSend['email'] =emailToSend['preview'] + 'Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. Eu sit tincidunt incorrupte definitionem, vis mutat affert percipit cu, eirmod consectetuer signiferumque eu per. In usu latine equidem dolores. Quo no falli viris intellegam, ut fugit veritus placerat per.
 Ius id vidit volumus mandamus, vide veritus democritum te nec, ei eos debet libris consulatu. No mei ferri graeco dicunt, ad cum veri accommodare. Sed at malis omnesque delicata, usu et iusto zzril meliore. Dicunt maiorum eloquentiam cum cu, sit summo dolor essent te. Ne quodsi nusquam legendos has, ea dicit voluptua eloquentiam pro, ad sit quas qualisque. Eos vocibus deserunt quaestio ei.
 Blandit incorrupte quaerendum in quo, nibh impedit id vis, vel no nullam semper audiam. Ei populo graeci consulatu mei, has ea stet modus phaedrum. Inani oblique ne has, duo et veritus detraxit. Tota ludus oratio ea mel, offendit persequeris ei vim. Eos dicat oratio partem ut, id cum ignota senserit intellegat. Sit inani ubique graecis ad, quando graecis liberavisse et cum, dicit option eruditi at duo. Homero salutatus suscipiantur eum id, tamquam voluptaria expetendis ad sed, nobis feugiat similique usu ex.
--- public/gmail.js
+++ public/gmail.js
@@ -1,58 +1,71 @@
 
-var printMail = function (index, element) {
-      $('#emails').prepend(
-      '<li>' + 
-      '<div class="checked" data-checked="false"></div>' + 
-      '<div class="starred" data-starred="' + element.starred + '"></div>' + 
-      '<div class="from">' + element.from + '</div>' + 
-      '<div class="subject">' + element.subject + '</div>' +
-      '<div class="preview">' + element.preview + '</div>' +
-      '</li>');
-	};
+$('#main-labels:first-child').click(checkForNewMails);
+function checkForNewMails () {
+	$.get('/email/new', representMails);
+}
 
-
-var representMails = function (data) {
+function representMails (data) {
 	$.each(data, printMail);
 }
 
-var getMails = function () {
-	$.get('/email/new', representMails );
-}
-$('#main-labels:first-child').click(getMails);
+function printMail (index, element) {
+  $('#emails').prepend(
+  '<li>' + 
+  '<div class="checked" data-checked="false"></div>' + 
+  '<div class="starred" data-starred="' + element.starred + '"></div>' + 
+  '<div class="from">' + element.from + '</div>' + 
+  '<div class="subject">' + element.subject + '</div>' +
+  '<div class="preview">' + element.preview + '</div>' +
+  '</li>');
+};
 
 
 
-var showMail = function (data) {
-	console.log(data);
-	$('#blank-email').after(
-      '<section class="completeMail">' + 
-	      '<div class="starred" data-starred="' + data.starred + '"></div>' + 
-	      '<div class="from">' + data.from + '</div>' + 
-	      '<div class="subject">' + data.subject + '</div>' +
-	      '<div class="preview">' + data.preview + '</div>' +
-	      '<div class="preview">' + data.preview + '</div>' +
-	      '<div class="preview">' + data.email + '</div>' +
-      '</section>');
-}
-
-$('#email').delegate('li', 'click', function () {
+$('#emails').delegate('li', 'click', function () {
 	$.get('/email/' + $(this).attr('data-id'), showMail);
 	console.log($(this).attr('data-id'));
 });
 
+function showMail (data) {
+	//console.log(data);
+	$('#footer').before(
+    '<section class="completeMail">' + 
+      '<div class="starred" data-starred="' + data.starred + '"></div>' + 
+      '<div class="from">' + data.from + '</div>' + 
+      '<div class="subject">' + data.subject + '</div>' +
+      '<div class="preview">' + data.preview + '</div>' +
+      '<div class="preview">' + data.preview + '</div>' +
+      '<div class="preview">' + data.email + '</div>' +
+    '</section>');
+}
 
 
 
 
-
-var sendMail = function () {
+$('#last-activity').click(createMail);
+function createMail () {
 	$.post('/email', function (data) {
 		$('#email').prepend( '<div class="sentConfirmation">' + data + '</div>' );
 	});
 }
-$('#last-activity').click(sendMail);
 
-var closeConfirmation = function () {
+
+$('#email').delegate('.sentConfirmation', 'click', closeConfirmation);
+function closeConfirmation () {
 	$(this).remove();
 };
-$('#email').delegate('.sentConfirmation', 'click', closeConfirmation);
+
+
+
+$('#newMail').click(newMail);
+function newMail () {
+	var pos = $('#footer').position();
+	$('#blank-email').css('display', 'block').css('top', pos.top-162);
+};
+
+
+
+$('.closeWindow').click(hideWindow);
+function hideWindow () {
+	$('#blank-email').css('display', 'none');
+}
